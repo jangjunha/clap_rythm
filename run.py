c7f33a05f324ff4e1810a3d1838c0469710ff672
Jang Junha <junha113@daum.net>
2016-02-13 00:36:46 +0900

    Redirect 'clap-rythm.appspot.com' to 'https://clap.heek.kr'

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+from flask import request, redirect
 from claprythm import create_app
+
 app = create_app('claprythm_config')
+
+@app.before_request
+def before_request():
+    if request.host.endswith('clap-rythm.appspot.com'):
+        return redirect('https://clap.heek.kr')
 
